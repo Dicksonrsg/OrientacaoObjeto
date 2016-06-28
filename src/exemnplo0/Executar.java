1ed332991c2b91e2bac21d0af0a1016cf8f56207
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-06-27 21:11:49 -0300

    1st commit

---
+++ src/exemnplo0/Executar.java
@@ -0,0 +1,16 @@
+
+package exemnplo0;
+
+import java.util.Scanner;
+
+public class Executar {
+    public static void main(String[] args) {
+        
+        Marca marca = new Marca(1, "Honda");
+        Modelo modelo = new Modelo(1, "Civic", marca);
+        Carro carro = new Carro(1, modelo, "XYZ-9999", 2010, 22000);
+        
+        System.out.println(carro);
+    }
+    
+}
---
+++ src/exemnplo0/Modelo.java
@@ -0,0 +1,57 @@
+
+package exemnplo0;
+
+
+public class Modelo {
+    
+    private int id;
+    private String nome;
+    private Marca marca;
+
+    public Modelo(int id, String nome, Marca marca) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+    }
+    
+    public Modelo(){
+    }
+
+    public Modelo(String nome) {
+        this.nome = nome;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo{" + "id=" + id + ", nome=" + nome + ", marca=" + marca + '}';
+    }
+    
+   
+    
+}
